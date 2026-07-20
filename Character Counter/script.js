f88e469eaf432ccd7749eb05ef478446209d3051
Ashwin-here01 <ashwin.online01@gmail.com>
2026-07-21 00:36:21 +0530

    Fixed Character counter project

--- Character Counter/script.js
+++ Character Counter/script.js
@@ -4,7 +4,7 @@ let count = document.querySelector("#count");
 text.addEventListener("input", () => {
     let InnerText = text.value;
     count.innerText = InnerText.length;
-    if(innerText.length == 200) {
+    if(InnerText.length === 200) {
         count.style.color = "red";
     } else {
         count.style.color = "black";
